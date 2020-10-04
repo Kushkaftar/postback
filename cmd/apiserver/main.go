e399b3ae3bc5726fa07e18193231fe8eaa73f5cb
kushkaftar <kushkaftar@gmail.com>
2020-10-04 21:34:21 +0300

    first commit

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/Kushkaftar/postback/internal/app/apiserver"
+)
+
+func main() {
+	s := apiserver.New()
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
